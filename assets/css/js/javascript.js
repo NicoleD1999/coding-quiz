9b1c6fcb6a00c34b5b3d1570919858f036fdddd3
NicoleD1999 <nmd1018@yahoo.com>
2022-03-26 18:05:37 -0500

    worked with tutor to incorporate highscores display screen

--- assets/css/js/javascript.js
+++ assets/css/js/javascript.js
@@ -14,14 +14,14 @@ var button = document.querySelector("#initial-button");
 
 var timeLeft = 80;
 var finalScore = 100;
-
+var timeInterval;
 
 timer.setAttribute("style", "text-align: right;  margin-top:-35px;")
 initalSection.setAttribute("style", "text-align: center;")
 buttonDiv.setAttribute("style", "display:flex; justify-content:center; align-items:center;" )
 
 function startTime() {
-    var timeInterval = setInterval(function(){
+    timeInterval = setInterval(function(){
     timeLeft--;
     finalScore--;
     timer.textContent= "Time Left " + timeLeft;
@@ -58,12 +58,16 @@ function showQuestionOne (q) {
             if (q.rightAnswer === index ) {
                 var right = document.querySelector("#right-wrong");
                 right.textContent = "Correct!"
-                showQuestionOne(questionTwo);
+                showQuestionTwo(questionTwo);
             } 
             else {
                 var wrong = document.querySelector("#right-wrong");
                 wrong.textContent = "Wrong!"
-                showQuestionOne(questionTwo);
+                timeLeft=timeLeft-10;
+                timer.textContent=timeLeft;
+                finalScore=finalScore-10;
+                console.log(finalScore);
+                showQuestionTwo(questionTwo);
             }
 
         });
@@ -71,6 +75,11 @@ function showQuestionOne (q) {
 
 };
 
+var questionTwo = {
+    title: "The condition in an if / else statement is enclosed within ________.",
+    alternatives: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    rightAnswer: 2
+};
 
 
 function showQuestionTwo (q) {
@@ -84,9 +93,15 @@ function showQuestionTwo (q) {
 
         element.addEventListener('click', function(){
             if (q.rightAnswer === index ) {
+                showQuestionThree(questionThree);
+                var right = document.querySelector("#right-wrong");
+                right.textContent = "Correct!"
                 console.log("Correct Answer!");
             } 
             else {
+                showQuestionThree(questionThree);
+                var wrong = document.querySelector("#right-wrong");
+                wrong.textContent = "Wrong!"
                 console.log ("Wrong Answer!");
             }
 
@@ -94,25 +109,162 @@ function showQuestionTwo (q) {
     });
 
 }
-var submitEl = document.querySelector("#submit");
-function getUserInput() {
-    var input = document.getElementById('input').value;
-    var result = document.getElementById('final-score');
-    
-    if (input.length < 3) {
-        result.textContent = 'Please Enter your initals AND score!';
-        //alert('Username must contain at least 3 characters');
-    } else {
-        result.textContent = 'Your score is ' + input;
-        //alert(nameField);
-    }
-    }
 
-submitEl.addEventListener('click', function(){
-    event.preventDefault();
-    getUserInput();
+var questionThree = {
+    title: "Arrays in JavaScript can be used to store ______.",
+    alternatives: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    rightAnswer: 2
+};
+
+
+function showQuestionThree (q) {
     
-});
+    var titleDiv = document.querySelector("#first-question")
+    titleDiv.textContent = q.title;
+
+    var answers = document.querySelectorAll(".button-answers");
+    answers.forEach(function(element, index){
+        element.textContent=q.alternatives[index];
+
+        element.addEventListener('click', function(){
+            if (q.rightAnswer === index ) {
+                var right = document.querySelector("#right-wrong");
+                right.textContent = "Correct!"
+                showQuestionFour(questionFour);
+                console.log("Correct Answer!");
+            } 
+            else {
+                var wrong = document.querySelector("#right-wrong");
+                wrong.textContent = "Wrong!"
+                showQuestionFour(questionFour);
+                console.log ("Wrong Answer!");
+            }
+
+        });
+    });
+
+}
+
+var questionFour = {
+    title: "String values must be enclosed within ______ when being assigned to variables.",
+    alternatives: ["commas", "curly brackets", "quotes", "parentheses"],
+    rightAnswer: 1
+};
+
+
+function showQuestionFour (q) {
+    
+    var titleDiv = document.querySelector("#first-question")
+    titleDiv.textContent = q.title;
+
+    var answers = document.querySelectorAll(".button-answers");
+    answers.forEach(function(element, index){
+        element.textContent=q.alternatives[index];
+
+        element.addEventListener('click', function(){
+            if (q.rightAnswer === index ) {
+                showQuestionFive(questionFive);
+                var right = document.querySelector("#right-wrong");
+                right.textContent = "Correct!"
+                console.log("Correct Answer!");
+            } 
+            else {
+                showQuestionFive(questionFive);
+                var wrong = document.querySelector("#right-wrong");
+                wrong.textContent = "Wrong!"
+                console.log ("Wrong Answer!");
+            }
+
+        });
+    });
+
+}
+
+var questionFive = {
+    title: "A very useful tool used during development and debugging for printing content to the debugger is",
+    alternatives: ["JavaScript", "Terminal/Bash", "for loops", "console.log"],
+    rightAnswer: 3
+};
+var form = document.querySelector("#form-submit");
+form.setAttribute("style", "display: none;");
+
+function showQuestionFive (q) {
+    
+    var titleDiv = document.querySelector("#first-question")
+    titleDiv.textContent = q.title;
+    var result = document.querySelector('#final-score');
+    var answers = document.querySelectorAll(".button-answers");
+    answers.forEach(function(element, index){
+        element.textContent=q.alternatives[index];
+
+        element.addEventListener('click', function(){
+            if (q.rightAnswer === index ) {
+                var right = document.querySelector("#right-wrong");
+                right.textContent = "Correct!"
+                form.setAttribute("style", "display;")
+                section1.setAttribute("style", "display: none;")
+                result.textContent = 'Your score is ' + finalScore;
+                console.log("Correct Answer!");
+                clearInterval(timeInterval);
+            } 
+            else {
+                var wrong = document.querySelector("#right-wrong");
+                wrong.textContent = "Wrong!"
+                form.setAttribute("style", "display;")
+                section1.setAttribute("style", "display: none;")
+                result.textContent = 'Your score is ' + finalScore;
+                console.log ("Wrong Answer!");
+                clearInterval(timeInterval);
+            }
+
+        });
+    });
+
+}
+var initialsInput = document.querySelector("#input");
+
+
+function submitScore (){
+ var getScores= JSON.parse(localStorage.getItem("highscores")) || []
+ var scoreModel = {
+     initials: initialsInput.value, 
+     score: finalScore,
+
+ }
+ getScores.push(scoreModel)
+ window.localStorage.setItem("highscores", JSON.stringify(getScores))
+ var finalScreen = document.querySelector("#final-screen");
+ form.setAttribute("style", "display: none;");
+ finalScreen.removeAttribute("class");
+}
+function showScores(){
+    var getScores= JSON.parse(localStorage.getItem("highscores")) || []
+    getScores.forEach(function(showScore){
+        var scoreListItem = document.createElement("li")
+        scoreListItem.textContent = showScore.initials+": "+showScore.score;
+        var listDiv= document.getElementById("scores")
+        listDiv.append(scoreListItem)
+    })
+}
+showScores();
+var submitEl = document.querySelector("#submit");
+submitEl.onclick=submitScore
+// function getUserInput() {
+    
+//     if (input.length > 3) {
+//         result.textContent = 'Your score is ' + finalScore;
+//         //alert('Username must contain at least 3 characters');
+//     } else {
+//         result.textContent = 'Your score is ' + finalScore;
+//         //alert(nameField);
+//     }
+//     }
+
+// submitEl.addEventListener('click', function(){
+//     event.preventDefault();
+//     getUserInput();
+    
+// });
 
 
 
